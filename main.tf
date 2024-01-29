400d3ed4853b7cf0f413059b94748baf665c99ff
e-ian <ianemma70@gmail.com>
2024-01-29 15:57:34 +0300

    add security groups and variables

--- main.tf
+++ main.tf
@@ -5,8 +5,34 @@ provider "aws" {
 resource "aws_instance" "terra-prod" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.html
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+    
+
 
     tags = {
         Name = "terraform-prod"
     }
+}
+
+resource "aws_security_group" "instance" {
+    name = "prod_sg"
+
+    ingress {
+        from_port = var.server_port
+        to_port = var.server_port
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+variable "server_port" {
+  type        = number
+  default     = 8080
+  description = "The port the server will use for HTTP requests"
 }
