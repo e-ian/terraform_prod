e71c69b549c7b48a7f7c9055c81cc799530664f8
e-ian <ianemma70@gmail.com>
2024-01-29 16:40:03 +0300

    add auto scaling group and terraform output

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "terra-prod" {
-    ami = "ami-0c55b159cbfafe1f0"
+resource "aws_launch_configuration" "terra-prod" {
+    image_id = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+    security_groups = [aws_security_group.instance.id]
 
     user_data = <<-EOF
                 #!/bin/bash
@@ -31,8 +31,27 @@ resource "aws_security_group" "instance" {
     }
 }
 
+resource "aws_autoscaling_group" "terra-prod"{
+    launch_configuration = aws_launch_configuration.terra-prod.name
+
+    min_size = 2
+    max_size = 10
+
+    tag {
+        key = "Name"
+        value = "terraform-asg-prod"
+        propagate_at_launch = true
+    }
+
+}
+
 variable "server_port" {
   type        = number
   default     = 8080
   description = "The port the server will use for HTTP requests"
+}
+
+output "public_ip" {
+  value       = aws_instance.terra-prod.public_ip
+  description = "The public ip address of the webserver"
 }
