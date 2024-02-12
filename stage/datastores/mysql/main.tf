a3a9f302418868b3cb97b84138e5ff82fff55e4e
e-ian <ianemma70@gmail.com>
2024-02-12 14:59:34 +0300

    add terrform backend config to datastore

--- stage/datastores/mysql/main.tf
+++ stage/datastores/mysql/main.tf
@@ -7,12 +7,25 @@ resource "aws_db_instance" "mysqldb" {
     engine = "mysql"
     allocated_storage = 10
     instance_class = "db.t2.micro"
-    name = "mysql_database"
     username = "admin"
 
-    password = data.aws_secretmanager_secret_version.db_password.secret_string
+    # password = data.aws_secretsmanager_secret_version.db_password.secret_string
+    password = "hshdywnq2*&^"
 }
 
-data "aws_secretmanager_secret_version" "db_password" {
-    secret_id = "mysql-master-password-stage"
+# to be configured later on AWS secrets manager console
+
+# data "aws_secretsmanager_secret_version" "db_password" {
+#     secret_id = "mysql-master-password-stage"
+# }
+
+terraform {
+    backend "s3" {
+        bucket = "terra-prod-state"
+        key = "stage/datastores/mysql/terraform.tfstate"
+        region = "us-east-2"
+
+        dynamodb_table = "terraform-prod-locks"
+        encrypt = true
+    }
 }
